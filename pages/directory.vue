a3abb0823aa391f2d0f7c424e42e030af3d299e8
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-06-28 13:16:44 -0400

    fix mapstate error

--- pages/directory.vue
+++ pages/directory.vue
@@ -3,10 +3,7 @@
     <v-card-title class="indigo white--text text-h5">
       Madison County Directory
     </v-card-title>
-    <v-row
-      class="pa-4"
-      justify="space-between"
-    >
+    <v-row class="pa-4" justify="space-between">
       <v-col cols="5">
         <v-treeview
           :active.sync="active"
@@ -28,9 +25,7 @@
 
       <v-divider vertical></v-divider>
 
-      <v-col
-        class="d-flex text-center"
-      >
+      <v-col class="d-flex text-center">
         <v-scroll-y-transition mode="out-in">
           <div
             v-if="!selected"
@@ -47,10 +42,7 @@
             max-width="400"
           >
             <v-card-text>
-              <v-avatar
-                v-if="avatar"
-                size="88"
-              >
+              <v-avatar v-if="avatar" size="88">
                 <v-img
                   :src="`https://avataaars.io/${avatar}`"
                   class="mb-6"
@@ -67,36 +59,20 @@
               </div>
             </v-card-text>
             <v-divider></v-divider>
-            <v-row
-              class="text-left"
-              tag="v-card-text"
-            >
-              <v-col
-                class="text-right mr-4 mb-2"
-                tag="strong"
-                cols="5"
-              >
+            <v-row class="text-left" tag="v-card-text">
+              <v-col class="text-right mr-4 mb-2" tag="strong" cols="5">
                 Company:
               </v-col>
               <v-col>{{ selected.company.name }}</v-col>
-              <v-col
-                class="text-right mr-4 mb-2"
-                tag="strong"
-                cols="5"
-              >
+              <v-col class="text-right mr-4 mb-2" tag="strong" cols="5">
                 Website:
               </v-col>
               <v-col>
-                <a
-                  :href="`//${selected.website}`"
-                  target="_blank"
-                >{{ selected.website }}</a>
+                <a :href="`//${selected.website}`" target="_blank">{{
+                  selected.website
+                }}</a>
               </v-col>
-              <v-col
-                class="text-right mr-4 mb-2"
-                tag="strong"
-                cols="5"
-              >
+              <v-col class="text-right mr-4 mb-2" tag="strong" cols="5">
                 Phone:
               </v-col>
               <v-col>{{ selected.phone }}</v-col>
@@ -109,66 +85,64 @@
 </template>
 
 <script>
-  const avatars = [
-    '?accessoriesType=Blank&avatarStyle=Circle&clotheColor=PastelGreen&clotheType=ShirtScoopNeck&eyeType=Wink&eyebrowType=UnibrowNatural&facialHairColor=Black&facialHairType=MoustacheMagnum&hairColor=Platinum&mouthType=Concerned&skinColor=Tanned&topType=Turban',
-    '?accessoriesType=Sunglasses&avatarStyle=Circle&clotheColor=Gray02&clotheType=ShirtScoopNeck&eyeType=EyeRoll&eyebrowType=RaisedExcited&facialHairColor=Red&facialHairType=BeardMagestic&hairColor=Red&hatColor=White&mouthType=Twinkle&skinColor=DarkBrown&topType=LongHairBun',
-    '?accessoriesType=Prescription02&avatarStyle=Circle&clotheColor=Black&clotheType=ShirtVNeck&eyeType=Surprised&eyebrowType=Angry&facialHairColor=Blonde&facialHairType=Blank&hairColor=Blonde&hatColor=PastelOrange&mouthType=Smile&skinColor=Black&topType=LongHairNotTooLong',
-    '?accessoriesType=Round&avatarStyle=Circle&clotheColor=PastelOrange&clotheType=Overall&eyeType=Close&eyebrowType=AngryNatural&facialHairColor=Blonde&facialHairType=Blank&graphicType=Pizza&hairColor=Black&hatColor=PastelBlue&mouthType=Serious&skinColor=Light&topType=LongHairBigHair',
-    '?accessoriesType=Kurt&avatarStyle=Circle&clotheColor=Gray01&clotheType=BlazerShirt&eyeType=Surprised&eyebrowType=Default&facialHairColor=Red&facialHairType=Blank&graphicType=Selena&hairColor=Red&hatColor=Blue02&mouthType=Twinkle&skinColor=Pale&topType=LongHairCurly',
-  ]
+const avatars = [
+  "?accessoriesType=Blank&avatarStyle=Circle&clotheColor=PastelGreen&clotheType=ShirtScoopNeck&eyeType=Wink&eyebrowType=UnibrowNatural&facialHairColor=Black&facialHairType=MoustacheMagnum&hairColor=Platinum&mouthType=Concerned&skinColor=Tanned&topType=Turban",
+  "?accessoriesType=Sunglasses&avatarStyle=Circle&clotheColor=Gray02&clotheType=ShirtScoopNeck&eyeType=EyeRoll&eyebrowType=RaisedExcited&facialHairColor=Red&facialHairType=BeardMagestic&hairColor=Red&hatColor=White&mouthType=Twinkle&skinColor=DarkBrown&topType=LongHairBun",
+  "?accessoriesType=Prescription02&avatarStyle=Circle&clotheColor=Black&clotheType=ShirtVNeck&eyeType=Surprised&eyebrowType=Angry&facialHairColor=Blonde&facialHairType=Blank&hairColor=Blonde&hatColor=PastelOrange&mouthType=Smile&skinColor=Black&topType=LongHairNotTooLong",
+  "?accessoriesType=Round&avatarStyle=Circle&clotheColor=PastelOrange&clotheType=Overall&eyeType=Close&eyebrowType=AngryNatural&facialHairColor=Blonde&facialHairType=Blank&graphicType=Pizza&hairColor=Black&hatColor=PastelBlue&mouthType=Serious&skinColor=Light&topType=LongHairBigHair",
+  "?accessoriesType=Kurt&avatarStyle=Circle&clotheColor=Gray01&clotheType=BlazerShirt&eyeType=Surprised&eyebrowType=Default&facialHairColor=Red&facialHairType=Blank&graphicType=Selena&hairColor=Red&hatColor=Blue02&mouthType=Twinkle&skinColor=Pale&topType=LongHairCurly"
+];
 
-  const pause = ms => new Promise(resolve => setTimeout(resolve, ms))
+const pause = ms => new Promise(resolve => setTimeout(resolve, ms));
+import { mapState } from "vuex";
 
-  export default {
-    data: () => ({
-      active: [],
-      avatar: null,
-      open: [],
-      users: [],
-    }),
+export default {
+  data: () => ({
+    active: [],
+    avatar: null,
+    open: [],
+    users: []
+  }),
 
-    computed: {
-        ...mapState([
-            'offices',]),
-      items () {
-        return [
-          {
-            name: 'Users',
-            children: this.users,
-          },
-        ]
-      },
-      selected () {
-        if (!this.active.length) return undefined
-
-        const id = this.active[0]
-
-        return this.users.find(user => user.id === id)
-      },
+  computed: {
+    ...mapState(["offices"]),
+    items() {
+      return [
+        {
+          name: "Users",
+          children: this.users
+        }
+      ];
     },
+    selected() {
+      if (!this.active.length) return undefined;
 
-    watch: {
-      selected: 'randomAvatar',
+      const id = this.active[0];
+
+      return this.users.find(user => user.id === id);
+    }
+  },
+
+  watch: {
+    selected: "randomAvatar"
+  },
+
+  methods: {
+    async fetchUsers(item) {
+      // Remove in 6 months and say
+      // you've made optimizations! :)
+      await pause(1500);
+
+      return fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(json => item.children.push(...json))
+        .catch(err => console.warn(err));
     },
-
-    methods: {
-      async fetchUsers (item) {
-        // Remove in 6 months and say
-        // you've made optimizations! :)
-        await pause(1500)
-
-        return fetch('https://jsonplaceholder.typicode.com/users')
-          .then(res => res.json())
-          .then(json => (item.children.push(...json)))
-          .catch(err => console.warn(err))
-      },
-      randomAvatar () {
-        this.avatar = avatars[Math.floor(Math.random() * avatars.length)]
-      },
-    },
+    randomAvatar() {
+      this.avatar = avatars[Math.floor(Math.random() * avatars.length)];
+    }
   }
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
