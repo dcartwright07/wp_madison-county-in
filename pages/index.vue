9bfaa0d4727e920831094208a988da302294d5c8
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-07-07 14:39:09 -0400

    Update api functions to include directory and destinations

--- pages/index.vue
+++ pages/index.vue
@@ -6,55 +6,6 @@
   </section>
 </template>
 
-<style scoped>
-/* .homeHeroHeader {
-  z-index: 1;
-}
-.r-gradient {
-  background: rgb(255, 255, 255);
-  background: linear-gradient(
-    180deg,
-    rgba(255, 255, 255, 0) 0%,
-    rgba(0, 0, 0, 0.4) 100%
-  );
-}
-.homeHeroTitle {
-  position: absolute;
-  top: 0;
-} */
-/* .homeHeroHeader .v-window-item {
-  position: relative;
-}
-.backgroundhero {
-  position: absolute;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-} */
-/* .fade-enter, .fade-leave-to {
-  opacity: 0;
-}
-.h1 {
-  font-size: 4rem;
-  z-index: 6;
-  position: absolute;
-  top: 30%;
-}
-.HomeSubHeader {
-  font-size: 2.4rem;
-  font-weight: 300;
-  z-index: 6;
-  position: absolute;
-  top: 23.5%;
-}
-.fheight {
-  height: 100vh;
-}*/
-/*********************/
-</style>
-
 <script>
 import { mapActions } from "vuex";
 
@@ -83,3 +34,5 @@ export default {
   ])
 };
 </script>
+
+<style scoped></style>
--- store/wuapi.js
+++ store/wuapi.js
@@ -15,22 +15,33 @@ const getAuthorization = (config, axios) => {
 };
 
 export const state = () => ({
-  events: []
+  events: [],
+  directory: [],
+  destinations: []
 });
 
 export const mutations = {
   UPDATE_EVENTS: (state, array) => {
     state.events = array;
+  },
+  UPDATE_DIRECTORY: (state, array) => {
+    state.events = array;
+  },
+  UPDATE_DESTINATIONS: (state, array) => {
+    state.events = array;
   }
 };
 
 export const actions = {
-  async getEvents({ state, commit }) {
+  async getEvents({ commit }) {
     const auth = await getAuthorization(this.$config, this.$axios);
 
     const events = await this.$axios
       .get(
-        this.$config.wuApiUrl + "/event?organization_id=" + this.$config.orgId,
+        this.$config.wuApiUrl +
+          "/event?organization_id=" +
+          this.$config.orgId +
+          "&copromotions=1",
         {
           headers: {
             Authorization: "Bearer " + auth.data.access_token,
@@ -47,5 +58,59 @@ export const actions = {
         context.error(error);
       });
     commit("UPDATE_EVENTS", events);
+  },
+
+  async getDirectory({ commit }) {
+    const auth = await getAuthorization(this.$config, this.$axios);
+
+    const directory = await this.$axios
+      .get(
+        this.$config.wuApiUrl +
+          "/directory?organization_id=" +
+          this.$config.orgId +
+          "&copromotions=1",
+        {
+          headers: {
+            Authorization: "Bearer " + auth.data.access_token,
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+          }
+        }
+      )
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        console.log(error);
+        context.error(error);
+      });
+    commit("UPDATE_DIRECTORY", directory);
+  },
+
+  async getDestinations({ commit }) {
+    const auth = await getAuthorization(this.$config, this.$axios);
+
+    const destinations = await this.$axios
+      .get(
+        this.$config.wuApiUrl +
+          "/destinations?organization_id=" +
+          this.$config.orgId +
+          "&copromotions=1",
+        {
+          headers: {
+            Authorization: "Bearer " + auth.data.access_token,
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+          }
+        }
+      )
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        console.log(error);
+        context.error(error);
+      });
+    commit("UPDATE_DESTINATIONS", destinations);
   }
 };
