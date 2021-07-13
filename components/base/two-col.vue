7fcf4053b7794af7edc71b1e06e16aece4f734b5
Kmcgrego <Kmcgrego@users.noreply.github.com>
2021-07-13 05:03:41 -0400

    Adjusted design

--- components/base/two-col.vue
+++ components/base/two-col.vue
@@ -7,17 +7,27 @@
       class="flex-sm-column-reverse ma-0"
       :class="{
         'flex-md-row': index % 2 === 0,
-        'flex-md-row-reverse': index % 2 !== 0
+        'flex-md-row-reverse': index % 2 !== 0,
+        primary: index % 5 === 0,
+        lightgrey: index % 5 === 1,
+        hyperlink: index % 5 === 2,
+        lightgrey: index % 5 === 3
       }"
       justify="center"
     >
-      <v-col v-if="!category.posts.length < 1" cols="12" md="4">
-        <v-sheet rounded shaped class="icon-section">
-          <div class="mb-5">
-            <h2 class="section-name">
+      <v-col v-if="!category.posts.length < 1" cols="12" md="4" >
+        <v-sheet rounded shaped class="icon-section transparent" :class="{'text-left': index % 2 === 0, 'text-right': index % 2 !== 0}">
+          <div class="mb-5" :class="{
+            'text-right': index % 2 !== 0,
+            'text-left': index % 2 === 0,
+            
+          }">
+            <h2 class="section-name mb-10" :class="{'text-right ml-2': index % 2 === 0, 'text-left mr-2': index % 2 !== 0}">
               {{ category.name }}
             </h2>
+              
           </div>
+          <div :class="{'text-right mr-4': index % 2 !== 0, 'text-left ml-4': index % 2 === 0}" v-html="category.content"></div>
           <nuxt-link
             v-for="post in category.posts"
             :key="post.slug"
@@ -27,19 +37,13 @@
           >
             <v-tooltip bottom>
               <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  :class="{
-                    primary: index % 5 === 0,
-                    secondary: index % 5 === 1,
-                    hyperlink: index % 5 === 2,
-                    accent: index % 5 === 3
-                  }"
+                <v-btn                  
                   v-bind="attrs"
                   v-on="on"
-                  class="circular-reveal"
+                  class="square-reveal lightgrey"
                   width="100"
                   height="100"
-                  elevation="2"
+                  elevation="4"
                   fab
                   icon
                   :style="{ content: 'post.slug' }"
@@ -59,7 +63,7 @@
       </v-col>
 
       <v-col v-if="!category.posts.length < 1" cols="12" md="6">
-        <v-sheet class="full-height" elevation="4">
+        <v-sheet class="full-height transparent" elevation="4">
           <v-img
             height="100%"
             elevation="4"
@@ -104,39 +108,39 @@ export default {
   font-size: 3.125rem;
 }
 
-.circular-reveal {
+.square-reveal {
   width: 256px;
   height: 256px;
-  border-radius: 50%;
+  border-radius: 0;
   overflow: hidden;
   cursor: pointer;
+  img {
+    width: 100%;
+  }
+  &::after {
+    // content: '';
+    position: absolute;
+    background: rgba(149, 116, 215, 0.509);
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    font-size: 60px;
+    transition: transform 0.2s cubic-bezier(0, 0, 0.3, 1),
+      opacity 0.2s cubic-bezier(0, 0, 0.3, 1);
+    transform: rotate(10deg);
+    opacity: 0;
+  }
+
+  &:hover::after {
+    transform: rotate(0deg);
+    opacity: 1;
+  }
 }
 
-.circular-reveal img {
-  width: 100%;
-}
 
-.circular-reveal::after {
-  // content: '';
-  position: absolute;
-  background: rgba(149, 116, 215, 0.509);
-  width: 100%;
-  height: 100%;
-  left: 0;
-  top: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #fff;
-  font-size: 60px;
-  transition: transform 0.2s cubic-bezier(0, 0, 0.3, 1),
-    opacity 0.2s cubic-bezier(0, 0, 0.3, 1);
-  transform: rotate(10deg);
-  opacity: 0;
-}
-
-.circular-reveal:hover::after {
-  transform: rotate(0deg);
-  opacity: 1;
-}
 </style>
