9e820db4c95704a9222bfe40976eabc81c01c00e
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-07-13 17:34:58 -0400

    Update site name

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "%s - nuxt-madison",
-    title: "nuxt-madison",
+    titleTemplate: "%s - Madison County, IN",
+    title: "Madison County, IN",
     htmlAttrs: {
       lang: "en"
     },
