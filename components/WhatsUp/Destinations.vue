815c7d51aecf495aa20541ddae61ed168f0022c8
Kmcgrego <Kmcgrego@users.noreply.github.com>
2021-07-19 10:59:20 -0400

    change to the merge

--- components/WhatsUp/Destinations.vue
+++ components/WhatsUp/Destinations.vue
@@ -1,12 +1,10 @@
 <template>
-  <v-row class="hyperlink" no-gutters>
-		<div class="block-head text-center col-12 mb-5">
-        
-      <div>
-        <h2 class="head-line display-3 lightgrey--text d-inline-flex flex-column mb-16">
-         <small class="top-subheader accent--text text-center">Don't know where to go?</small>Let's Explore
-			  </h2>
-      </div>
+  <div>
+		<div class="d-flex justify-space-around mt-8 mb-10">
+
+			<h2 class="head-line display-3 text-center blackish--text">
+        <small class="top-subheader primary--text text-left">Don't know where to go?</small><br/>Let's Explore
+			</h2>
 			<!-- <p class="lead mt-2 head-desc primary--text">Upcoming events</p> -->
 		</div>
 
