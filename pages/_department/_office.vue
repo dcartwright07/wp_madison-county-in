e90272ef2f1ce5a3afdfaaaf4db62ce175e63270
Kmcgrego <Kmcgrego@users.noreply.github.com>
2021-07-21 13:45:05 -0400

    update to office and colour

--- pages/_department/_office.vue
+++ pages/_department/_office.vue
@@ -2,27 +2,33 @@
   <section>
     <BaseSubpageheader :office="office" />
 
-    <v-container>
-      <v-row class=" pa-5 d-flex flex-column">
-        <v-col
-          cols="12"
-          class="d-flex flex-md-row flex-sm-column-reverse lightgrey pa-10"
-        >
-          <div class="col-md-6 col-sm-12">
-            <div class="text-left mt-12 mb-6 text-center ">
-              <h2 class="h2">What We Do</h2>
-            </div>
-            <div tag="p" v-html="office.acf.services"></div>
-          </div>
-          <div class="col-md-6 col-sm-12">
-            <v-sheet elevation='5'>
-              <v-img  src="https://mcapi.signaturewebcreations.com/wp-content/uploads/2021/07/photo-1533219057257-4bb9ed5d2cc6.jpeg" height="500" />
-            </v-sheet>
-          </div>
-        </v-col>
-        <v-col class="section__content" v-html="office.acf.content"></v-col>
-      </v-row>
-    </v-container>
+   
+    <v-row class="d-flex flex-md-row flex-sm-column-reverse pa-10" v-for="(row,index) in office.acf.two_column_layout" :key="row.services"
+    :class="{
+        'flex-md-row': index % 2 === 0,
+        'flex-md-row-reverse ': index % 2 !== 0,
+        'lightgrey': index === 0,
+        'greyish lightgrey--text' : index % 5 === 1,
+        'lightgrey': index % 5 === 2,
+        'redish lightgrey--text': index % 5 === 3
+      }"
+    >
+    <!-- {{index === 0}} -->
+      <v-col class="col-md-6 col-sm-12 mt-3 "
+       :class="{
+            'text-md-left': index % 2 === 0,
+            'text-md-right': index % 2 !== 0
+          }"
+       >
+       <h2 v-html="row.two_column_header"/>
+       <div class="mt-7" v-html="row.services" />
+       </v-col>
+      <v-col class="col-md-6 col-sm-12">
+        <v-sheet elevation='5'>
+          <v-img :src="row.two_col_image" max-height="700"/>
+        </v-sheet>
+      </v-col>
+    </v-row>
    <!-- <BaseSidebar/> -->
 
     <v-row no-gutters v-if="profiles.length > 0">
@@ -98,5 +104,14 @@ export default {
 .h2 {
   font-size: 30px !important;
   font-weight: 400 !important;
+  &::after{
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 4px;
+    background-color: #ead11b;
+    bottom: -10px;
+    left: 0;
+  }
 }
 </style>
