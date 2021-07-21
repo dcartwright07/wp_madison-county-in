30d1e808b80de66e0f6a137fa05ba5f3d4c2baf2
Kmcgrego <Kmcgrego@users.noreply.github.com>
2021-07-21 16:41:08 -0400

    update to office and teams

--- components/base/team.vue
+++ components/base/team.vue
@@ -11,7 +11,7 @@
       <v-col v-for="(profile, index) in members" :key="profile.id" sm="3">
         <v-dialog v-model="dialog[index]" width="500">
           <template v-slot:activator="{ on, attrs }">
-            <v-avatar v-bind="attrs" v-on="on" size="128">
+            <v-avatar class="profile_av" v-bind="attrs" v-on="on" size="128">
               <v-img
                 v-if="profile.media_url"
                 cover
@@ -19,7 +19,9 @@
                 max-width="250"
                 :src="profile.media_url"
               />
+             
               <v-img
+                elevation="10"
                 v-else
                 cover
                 max-height="250"
@@ -28,7 +30,7 @@
               />
               <!-- <v-img :src="profile.media_url ? profile.media_url : require('~/assets/madison_silhouettes_1.jpg') "/> -->
             </v-avatar>
-          </template>
+          </template>    
           <v-card>
             <v-card-title
               v-html="profile.title"
@@ -115,7 +117,9 @@ export default {
     content: "";
     width: 100%;
     height: 2px;
-    background: "secondary";
   }
+}
+.profile_av{
+  box-shadow: 0 5px 5px #444;
 }
 </style>
