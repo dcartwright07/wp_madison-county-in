908efed64e2d4c5e8d4ed6ed044844d86c44c249
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-07-26 17:44:20 -0400

    Update event and destination pages

---
+++ components/base/tabs.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <div class="nav-wrapper">
+      <v-tabs
+        v-model="tab"
+        class="nav nav-pills nav-fill flex-column flex-md-row"
+        id="tabs-icons-text"
+        role="tablist"
+      >
+        <v-tab
+          v-for="(nav, index) in navigation"
+          :key="nav.name"
+          :id="
+            index === 0
+              ? `tabs-icons-text-${index + 1}-tab active show`
+              : `tabs-icons-text-${index + 1}-tab`
+          "
+          class="nav-link mb-sm-3 mb-md-0"
+          data-toggle="tab"
+          :href="`#tabs-icons-text-${index + 1}`"
+          role="tab"
+          :aria-controls="`tabs-icons-text-${index + 1}`"
+          :aria-selected="index === 0 ? 'true' : 'false'"
+        >
+          <i :class="`fa ${nav.icon} mr-2`"></i>{{ nav.name }}
+        </v-tab>
+      </v-tabs>
+    </div>
+
+    <v-card class="card shadow">
+      <v-card-text class="card-body mt-5">
+        <div class="tab-content" id="eventTabContent">
+          <v-tabs-items v-model="tab">
+            <v-tab-item id="tabs-icons-text-1">
+              <div class="tab-pane fade active show">
+                <p
+                  v-if="!item.content || item.content === ''"
+                  class="description"
+                  v-html="item.description"
+                />
+                <p v-else class="description" v-html="item.content" />
+              </div>
+            </v-tab-item>
+
+            <v-tab-item id="tabs-icons-text-2">
+              <div class="tab-pane fade">
+                <v-list two-line>
+                  <v-list-item-group active-class="primary--text">
+                    <template v-for="(event, index) in listOfEvents">
+                      <v-list-item
+                        :key="event.id"
+                        :to="{
+                          name: 'events-id',
+                          params: { id: event.id },
+                        }"
+                      >
+                        <template v-slot:default="{ active }">
+                          <v-list-item-content>
+                            <v-list-item-title v-text="event.title" />
+
+                            <v-list-item-subtitle
+                              class="text--primary"
+                              v-text="event.name"
+                            />
+                          </v-list-item-content>
+
+                          <v-list-item-action>
+                            <v-list-item-action-text>
+                              {{ event.start | formatDate($moment, "MMMM D") }}
+                            </v-list-item-action-text>
+
+                            <v-icon v-if="!active" color="grey lighten-1">
+                              mdi-star-outline
+                            </v-icon>
+
+                            <v-icon v-else color="yellow darken-3">
+                              mdi-star
+                            </v-icon>
+                          </v-list-item-action>
+                        </template>
+                      </v-list-item>
+
+                      <v-divider
+                        v-if="index < listOfEvents.length - 1"
+                        :key="index"
+                      ></v-divider>
+                    </template>
+                  </v-list-item-group>
+                </v-list>
+              </div>
+            </v-tab-item>
+          </v-tabs-items>
+        </div>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    navigation: {
+      type: Array,
+      required: true,
+    },
+    item: {
+      type: Object,
+      required: true,
+    },
+    listOfEvents: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      tab: null,
+    }
+  },
+}
+</script>
--- pages/events/_id.vue
+++ pages/events/_id.vue
@@ -31,114 +31,20 @@
                 {{ item.name }}
               </v-btn>
             </div>
-            <div class="nav-wrapper">
-              <v-tabs
-                v-model="tab"
-                class="nav nav-pills nav-fill flex-column flex-md-row"
-                id="tabs-icons-text"
-                role="tablist"
-              >
-                <v-tab
-                  class="nav-link mb-sm-3 mb-md-0 active show"
-                  id="tabs-icons-text-1-tab"
-                  data-toggle="tab"
-                  href="#tabs-icons-text-1"
-                  role="tab"
-                  aria-controls="tabs-icons-text-1"
-                  aria-selected="true"
-                >
-                  <i class="fa fa-info-circle mr-2"></i>About
-                </v-tab>
 
-                <v-tab
-                  class="nav-link mb-sm-3 mb-md-0"
-                  id="tabs-icons-text-2-tab"
-                  data-toggle="tab"
-                  href="#tabs-icons-text-2"
-                  role="tab"
-                  aria-controls="tabs-icons-text-2"
-                  aria-selected="false"
-                >
-                  <i class="fa fa-calendar-alt mr-2"></i>Related Events
-                </v-tab>
-              </v-tabs>
-            </div>
-
-            <v-card class="card shadow">
-              <v-card-text class="card-body mt-5">
-                <div class="tab-content" id="eventTabContent">
-                  <v-tabs-items v-model="tab">
-                    <v-tab-item id="tabs-icons-text-1">
-                      <div class="tab-pane fade active show">
-                        <p
-                          v-if="!event.content || event.content === ''"
-                          class="description"
-                          v-html="event.description"
-                        />
-                        <p v-else class="description" v-html="event.content" />
-                      </div>
-                    </v-tab-item>
-
-                    <v-tab-item id="tabs-icons-text-2">
-                      <div class="tab-pane fade">
-                        <v-list two-line>
-                          <v-list-item-group
-                            v-model="selected"
-                            active-class="pink--text"
-                            multiple
-                          >
-                            <template v-for="(item, index) in listOfEvents">
-                              <v-list-item :key="item.id" :to="{ name: 'events-id', params: { id: item.id } }"  >
-                                <template v-slot:default="{ active }" >
-                                  <v-list-item-content>
-                                    <v-list-item-title
-                                      v-text="item.title"
-                                    ></v-list-item-title>
-
-                                    <v-list-item-subtitle
-                                      class="text--primary"
-                                      v-text="item.name"
-                                    ></v-list-item-subtitle>
-
-                                    <!-- <v-list-item-subtitle
-                                      v-text="item.subtitle"
-                                    ></v-list-item-subtitle> -->
-                                  </v-list-item-content>
-
-                                  <v-list-item-action>
-                                    <v-list-item-action-text>{{ item.start | formatDate($moment, "MMMM D") }}</v-list-item-action-text>
-
-                                    <v-icon
-                                      v-if="!active"
-                                      color="grey lighten-1"
-                                    >
-                                      mdi-star-outline
-                                    </v-icon>
-
-                                    <v-icon v-else color="yellow darken-3">
-                                      mdi-star
-                                    </v-icon>
-                                  </v-list-item-action>
-                                </template>
-                              </v-list-item>
-
-                              <!-- <v-divider
-                                v-if="index < items.length - 1"
-                                :key="index"
-                              ></v-divider> -->
-                            </template>
-                          </v-list-item-group>
-                        </v-list>
-                      </div>
-                    </v-tab-item>
-                  </v-tabs-items>
-                </div>
-              </v-card-text>
-            </v-card>
+            <BaseTabs
+              :navigation="tabs"
+              :item="event"
+              :listOfEvents="listOfEvents"
+            />
           </div>
         </v-col>
         <v-col md="3" lg="3" class="pt-md-16">
-          <BaseSidebar />
+          <BaseSidebar
+            :email="event.contact_email"
+            :phone="event.contact_phone"
+            :c_name="event.contact_name"
+          />
         </v-col>
       </v-row>
     </v-container>
@@ -151,12 +57,21 @@ import { mapState, mapActions } from "vuex"
 export default {
   data() {
     return {
-      tab: null,
+      tabs: [
+        {
+          name: "About",
+          icon: "fa-info-circle",
+        },
+        {
+          name: "Related Events",
+          icon: "fa-calendar-alt",
+        },
+      ],
     }
   },
 
   computed: mapState({
-	categories: (state) => state.wuapi.event_categories,
+    categories: (state) => state.wuapi.event_categories,
     event: (state) => state.wuapi.event,
     listOfEvents: (state) => state.wuapi.latestEvents,
   }),
@@ -165,21 +80,20 @@ export default {
 
   async created() {
     await this.getEvent(this.$route.params.id)
-	let relatedCategories = ''
-	this.event.categories.forEach( (element, index) =>{
-		relatedCategories += this.categories.find(event => event.name === element.name).key
-		if (index < this.event.categories.length-1){
-			relatedCategories +=  ','
-		}
-
-	
-
-	});
+    let relatedCategories = ""
+    this.event.categories.forEach((element, index) => {
+      relatedCategories += this.categories.find(
+        (event) => event.name === element.name
+      ).key
+      if (index < this.event.categories.length - 1) {
+        relatedCategories += ","
+      }
+    })
 
     let options = {
       type: "latest",
       limit: "5",
-	  categories: relatedCategories,
+      categories: relatedCategories,
     }
     this.getEvents(options)
   },
