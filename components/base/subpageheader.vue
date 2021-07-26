03bb23a73d365cd9110fcb109d89f7261fa7780a
Kmcgrego <Kmcgrego@users.noreply.github.com>
2021-07-26 14:29:06 -0400

    Add subheader image conditional

--- components/base/subpageheader.vue
+++ components/base/subpageheader.vue
@@ -2,6 +2,8 @@
   <v-sheet
     class="theme--light grey lighten-3"
     style="height: 20vh; min-height: 600px;"
+    :style="office.media_url != '' ? 'background-image: url(' + office.media_url + ');' :  'background-image: url(http://mcapi.signaturewebcreations.com/wp-content/uploads/2021/07/photo-1602992708529-c9fdb12905c9-scaled.jpeg);'
+     "
   >
     <v-overlay absolute style="margin:0; z-index:0; ">
       <v-container>
@@ -10,8 +12,8 @@
             <h1 class="display-2 lightgrey--text text-left" v-html="office.name"></h1>
           </v-col>
           <v-col cols="12" md="6">
-            {{ office.acf.description }}
-          </v-col>
+            <span v-html="office.acf.description"/>
+          </v-col>∫
         </v-row>
       </v-container>
     </v-overlay>
@@ -31,7 +33,7 @@ export default {
 
 <style lang="scss" scoped>
 .v-sheet.theme--light.grey.lighten-3{
-  background-image: url(http://mcapi.signaturewebcreations.com/wp-content/uploads/2021/07/photo-1602992708529-c9fdb12905c9-scaled.jpeg);
+  // background-image: url(http://mcapi.signaturewebcreations.com/wp-content/uploads/2021/07/photo-1602992708529-c9fdb12905c9-scaled.jpeg);
   background-size:  cover;
   background-position: center;
   position: relative;
